4ffe21c090dda8eff4d7e6300fb5afe54530dacb
Ashot Khachatryan <ashotkhachatryanh@gmail.com>
2022-04-08 13:57:18 +0400

    Initial commit

---
+++ CustomerApp/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using CustomerData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CustomerApp
+{
+    public partial class MainPage : UserControl
+    {
+        private DataManager manager = new DataManager();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            myDataGrid.ItemsSource = manager.Customers;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox.Text))
+            {
+                MessageBox.Show("The name cannot be empty.");
+                return;
+            }
+            UpdateData();
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            checkBox.IsChecked = false;
+            textBox.Text = "";
+            UpdateData();
+        }
+
+        private void UpdateData()
+        {
+            manager.UpdateData(textBox.Text, checkBox.IsChecked);
+        }
+    }
+}
---
+++ CustomerData/DataManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CustomerData
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsMember { get; set; }
+    }
+
+    public class DataManager
+    {
+        private readonly ObservableCollection<Customer> _customers = new ObservableCollection<Customer>
+        {
+            new Customer { FirstName = "William",  LastName = "Harris",  Email = "william@opensilver.net",  IsMember = true },
+            new Customer { FirstName = "Robert",   LastName = "Garza",   Email = "robert1@opensilver.net",  IsMember = false },
+            new Customer { FirstName = "Ian",      LastName = "Gates",   Email = "ian@opensilver.net",      IsMember = false },
+            new Customer { FirstName = "Lior",     LastName = "Caprio",  Email = "lior1@opensilver.net",    IsMember = true },
+            new Customer { FirstName = "Robert",   LastName = "Feldman", Email = "robert2@opensilver.net",  IsMember = false },
+            new Customer { FirstName = "Scarlett", LastName = "Koller",  Email = "scarlett@opensilver.net", IsMember = true },
+            new Customer { FirstName = "Prag",     LastName = "Smith",   Email = "prag@opensilver.net",     IsMember = true },
+            new Customer { FirstName = "Lior",     LastName = "Field",   Email = "lior2@opensilver.net",    IsMember = false }
+        };
+
+        public ObservableCollection<Customer> Customers { get; }
+
+        public DataManager()
+        {
+            Customers = new ObservableCollection<Customer>(_customers);
+        }
+
+        private void Reset()
+        {
+            Customers.Clear();
+            foreach (var i in _customers)
+            {
+                Customers.Add(i);
+            }
+        }
+
+        public void UpdateData(string name, bool? is_member)
+        {
+            Reset();
+
+            if (!String.IsNullOrEmpty(name))
+            {
+                var items = Customers.Where(x => x.FirstName != name).ToList();
+                foreach (var i in items)
+                {
+                    Customers.Remove(i);
+                }
+            }
+
+            if (is_member.HasValue && is_member.Value)
+            {
+                var items = Customers.Where(x => !x.IsMember).ToList();
+                foreach (var i in items)
+                {
+                    Customers.Remove(i);
+                }
+            }
+        }
+    }
+}
